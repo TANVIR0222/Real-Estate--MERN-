cc1df42ca0412029a78947889c65004ddb49adcc
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-11 14:52:47 +0600

    React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <p className="read-the-docs text-black bg-red-50">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
